119ba379a5ae694033d3e9727038791022ec6fdf
Ryo2034 <31241279+RF2034@users.noreply.github.com>
2024-09-17 14:36:21 +0900

    微細な修正

--- src/app/components/Projects.tsx
+++ src/app/components/Projects.tsx
@@ -3,7 +3,7 @@ import projectsData from "../data/projects";
 export default function Projects() {
   return (
     <section className="py-10">
-      <h2 className="text-3xl font-bold text-center mb-6">プロジェクト</h2>
+      <h2 className="text-3xl font-bold text-center mb-6">Projects</h2>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
         {projectsData.map((project, index) => (
           <div key={index} className="card bg-base-100 shadow-xl">
--- src/app/components/Skills.tsx
+++ src/app/components/Skills.tsx
@@ -4,7 +4,7 @@ export default function Skills() {
   return (
     <section className="py-10">
       <h2 className="text-3xl font-bold text-center mb-6 text-base-content">
-        スキル
+        Skills
       </h2>
       <div className="flex flex-wrap justify-center gap-4 text-base-content">
         {skillsData.map((skill, index) => (
